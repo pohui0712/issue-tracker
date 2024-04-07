ee1d8ababcdf7533680b7965745e2d43a3ff5f0b
pohui0712 <phxx04@gmail.com>
2024-04-08 00:24:56 +0800

    Refactor: Apply the Single Responsibility Prinsiple (SRP)

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,24 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Card, Heading, Text } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+
+interface Props {
+  issue: Issue;
+}
+const IssueDetails = ({ issue }: Props) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <div className="space-x-3 my-2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </div>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
