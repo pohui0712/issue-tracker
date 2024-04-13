a5a236113f5b2c8007f258837600f5f524529f6d
pohui0712 <phxx04@gmail.com>
2024-04-14 00:48:28 +0800

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,37 +2,55 @@ import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
+import { patchIssueSchema } from "@/app/validationSchemas";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   // to get the current user session
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (!session) return NextResponse.json({}, { status: 401 });
+  // if (!session) return NextResponse.json({}, { status: 401 });
 
   // validation the body of request
   const body = await request.json();
 
-  /** 
-    ----> Not need to apply as 'IssueForm' is alraady use 'schema' to validate
-    // // vadiation with shemca
-    // const validation = issueSchema.safeParse(body);
+  // ----> Not need to apply as 'IssueForm' is alraady use 'schema' to validate
+  // vadiation with shemca
+  const validation = patchIssueSchema.safeParse(body);
 
-    // if (!validation.success)
-    //   return NextResponse.json(validation.error.format(), { status: 400 });
-  **/
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  // check if thre is a user id in the body of the request
+  const { assignedToUserId, title, description, status } = body;
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+
+    if (!user)
+      return NextResponse.json({ error: "Invalid error" }, { status: 400 });
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
 
   // Update with specific id
   const updateIssue = await prisma.issue.update({
     where: {
-      id: parseInt(params.id),
+      id: issue.id,
     },
     data: {
-      title: body.title,
-      description: body.description,
-      status: body.status,
+      title,
+      description,
+      assignedToUserId,
+      status,
     },
   });
 
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -2,5 +2,21 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
   title: z.string().min(1, "Tittle is required.").max(255),
-  description: z.string().min(1, "Description is required."),
+  description: z.string().min(1, "Description is required.").max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Tittle is required.").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
+  status: z.string().min(1, "Choose atleast one of the status").optional(),
 });
