17ac2231b1e8689a9d51470858a35ade7b46d4cc
pohui0712 <phxx04@gmail.com>
2024-04-14 21:20:11 +0800

    Fix the filtering hydration problem

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -46,22 +46,23 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
-          {columns.map((column) => (
-            <Table.ColumnHeaderCell
-              key={column.value}
-              className={column.className}
-            >
-              <NextLink
-                href={{ query: { ...searchParams, orderBy: column.value } }}
+          <Table.Row>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
               >
-                {column.label}
-              </NextLink>
-              {column.value === searchParams.orderBy && (
-                <ArrowUpIcon className="inline" />
-              )}
-            </Table.ColumnHeaderCell>
-          ))}
-          <Table.Row></Table.Row>
+                <NextLink
+                  href={{ query: { ...searchParams, orderBy: column.value } }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
+          </Table.Row>
         </Table.Header>
         <Table.Body>
           {issues.map((issue) => (
