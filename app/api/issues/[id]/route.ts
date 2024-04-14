965c1a6647dd4fb18a1d6c9274bbde82cc6edb27
pohui0712 <phxx04@gmail.com>
2024-04-14 14:32:24 +0800

    Assign an issue to user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -9,14 +9,13 @@ export async function PATCH(
   { params }: { params: { id: string } }
 ) {
   // to get the current user session
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  // if (!session) return NextResponse.json({}, { status: 401 });
+  if (!session) return NextResponse.json({}, { status: 401 });
 
   // validation the body of request
   const body = await request.json();
 
-  // ----> Not need to apply as 'IssueForm' is alraady use 'schema' to validate
   // vadiation with shemca
   const validation = patchIssueSchema.safeParse(body);
 
